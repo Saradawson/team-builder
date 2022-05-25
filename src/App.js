58c5b2c5ca4475d177b0d1a0cc58f0b17a4f0a14
unknown <saradawsone@gmail.com>
2022-05-24 22:48:33 -0700

    state setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import MemberForm from './components/Form';
 import './App.css';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  
+  const [values, setValues] = useState({name: '', email: '', role: ''});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Builder</h1>
+      <MemberForm 
+      />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function MemberForm(props) {
+
+    return (
+        <form>
+            <label>Name
+                <input
+                    type='text'
+                    name='name'
+                    
+                />
+            </label>
+        </form>
+    )
+}
