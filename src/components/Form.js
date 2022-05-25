f51312a0a5e7a1701ab3d85fa27e7d5ace651418
unknown <saradawsone@gmail.com>
2022-05-24 23:09:16 -0700

    form is looking good so far

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,12 +4,27 @@ export default function MemberForm(props) {
 
     return (
         <form>
-            <label>Name
+            <label>Name:
                 <input
                     type='text'
                     name='name'
-                    
+                    placeholder='Type your name here!'
                 />
+            </label>
+            <label>Email:
+                <input
+                    type='email'
+                    name='email'
+                    placeholder='Type your email here!'
+                />
+            </label>
+            <label>Role:
+                <select name='role'>
+                    <option value=''>-- Select a Role --</option>
+                    <option value='Backend Engineer'>Backend Engineer</option>
+                    <option value='Frontend Engineer'>Frontend Engineer</option>
+                    <option value='Designer'>Designer</option>
+                </select>
             </label>
         </form>
     )
